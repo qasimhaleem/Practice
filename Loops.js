e17cf390f199cae0ebbc43a33924f3183b1a8f43
Qasim Haleem <mrqasimhaleem@gmail.com>
2025-09-25 16:55:26 +0500

    Loops finished

---
+++ Loops.js
@@ -0,0 +1,56 @@
+/* for(i=0; i<10; i++) {
+    console.log(i);
+}
+ */
+/* let data = 19;
+while(data>=2) {
+    console.log(data);
+    data = data-2;
+} */
+/* 
+let num = 35;
+if(num %2 === 0) {
+    console.log("you have chosen the even number")
+}if(num %2 === 1){
+    console.log("remainder deos not equall to 0")
+} */
+
+/* let i = 10;
+do{
+    console.log("Count of Loop: " + i)
+    i--;
+}while(i>=1);
+ */
+/* 
+let person = {Name: "Qasim", Age: 19, Geneder: "Male"};
+for(let key in person) {
+    console.log(key +": " + person[key]);
+} */
+
+/* let data = {copy: "feroz", book: "D.E", Pen: "Piono"};
+for(let item in data) {
+    console.log(item + ": " + data[item]);
+} */
+
+/* 
+let product = ["mouse", "keybord", "LCD"];
+for(let item of product) {
+    console.log(item);
+} */
+
+
+/* const todo = ["Lock", "PartsOfMony", "AdeelRoomLock"];
+for(let item of todo) {
+    console.log(item);
+} */
+
+/* const arr = [2,3,5,2,5];
+arr.forEach(item => {
+    console.log(item);
+    
+}); */
+
+function addition(a,b){
+    return a + b;
+}
+console.log(addition(4, 10));
--- source.js
+++ source.js
@@ -62,3 +62,4 @@ switch(form) {
     default : 
         console.log("You are not eligeble");
     }
+
