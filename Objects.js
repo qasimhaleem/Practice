2c067ef9f730c397ef18d65dfae3c0a30c6dfbe6
Qasim Haleem <mrqasimhaleem@gmail.com>
2025-09-27 19:23:49 +0500

    Object Finished

---
+++ Objects.js
@@ -0,0 +1,101 @@
+/* 
+const obj = {
+    name: "Qasim",
+    age: 22,
+    address: {
+        city: "thall",
+        district: "Hangu",
+        province: "kpk",
+        postalCode: 26270
+    }
+}
+console.log(obj.address.postalCode); */
+
+/* const data = {
+    mars: 95,
+    name: "Qasim",
+    gpa: 3.75,
+    ispass: true,
+    friends: ['hamza','waqas', 'imdad'],
+    address: {
+        city: "Thall",
+        district: "Hangu",
+        postalCode: "26270"
+    }
+}
+
+if(data.ispass==true) {
+    console.log(data.friends);
+} */
+
+
+/* const arr = [
+    obj1 = {
+        name: "Qasim",
+        age: 22,
+        gpa: 3.75,
+        ispass: false
+    },
+    obj2 = {
+        name: "Hamza",
+        age: 21,
+        gpa: 3.69,
+        ispass: true
+    },
+    obj3 = {
+        name: "Waqas",
+        age: 26,
+        gpa: 3.70,
+        ispass: true        
+    }
+]
+
+arr.forEach(item => {
+    if(item.ispass === true) {
+        console.log(item.name);
+    }
+}) */
+
+/* const stu = {
+    name: "Qasim",
+    age: 22,
+    cit: "thall"
+}
+const data = JSON.stringify(stu)
+console.log(data); */
+
+
+/* const user = new Object();
+user.name = "Qasim";
+console.log(user);
+ */
+
+/* const obj = {
+    name: "Qasim",
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+obj.greet(); */
+
+
+/* const student_list = {
+    mars: 91,
+    name: "Qasim",
+    age: 22,
+    ispass: true,
+    friends: ['Hamze', 'waqas', 'adeel'],
+    address: {
+        city: "Thal",
+        district: "Hangu"
+    }
+}
+const {name, address} = student_list;
+const {city, district} = student_list.address;
+function greet(names) {
+    name1 = names
+    console.log(`Hello ${name1}`);
+}
+greet(name)
+console.log(district); */
+
